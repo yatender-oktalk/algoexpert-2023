50d66e8621098233828e503f614bbad91441451e
Yatender Singh <yatender.nitk@gmail.com>
2023-08-24 14:59:29 +0530

    Adding the merge sort

--- sorting/merge/merge_sort.go
+++ sorting/merge/merge_sort.go
@@ -1,5 +1,38 @@
 package merge
 
 func MergeSort(arr []int) []int {
-	return arr
+	if len(arr) > 1 {
+		firstHalf := MergeSort(arr[:len(arr)/2])
+		secondHalf := MergeSort(arr[len(arr)/2:])
+		return Merge(firstHalf, secondHalf)
+	} else {
+		return arr
+	}
+}
+
+func Merge(firstHalf, secondHalf []int) []int {
+	sortedArr := []int{}
+	i := 0
+	j := 0
+
+	for i < len(firstHalf) && j < len(secondHalf) {
+		if firstHalf[i] <= secondHalf[j] {
+			sortedArr = append(sortedArr, firstHalf[i])
+			i++
+		} else {
+			sortedArr = append(sortedArr, secondHalf[j])
+			j++
+		}
+	}
+
+	for i < len(firstHalf) {
+		sortedArr = append(sortedArr, firstHalf[i])
+		i++
+	}
+
+	for j < len(secondHalf) {
+		sortedArr = append(sortedArr, secondHalf[j])
+		j++
+	}
+	return sortedArr
 }
--- sorting/merge/merge_sort_test.go
+++ sorting/merge/merge_sort_test.go
@@ -3,7 +3,7 @@ package merge
 import "testing"
 
 func TestMergeSorting(t *testing.T) {
-	want := []int{1, 1, 2, 3, 3, 4, 5}
+	want := []int{1, 1, 2, 3, 3, 4, 5, 6}
 	got := MergeSort([]int{5, 4, 3, 2, 1, 3, 1})
 	if len(got) != len(want) {
 		t.Errorf("got %v want %v", got, want)
